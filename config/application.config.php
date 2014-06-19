898982b5aefb20e177c7015290fd4751e97e220c
root <root@91-234-32-103.thehost.com.ua>
2014-06-19 14:51:10 +0300

    Add icons in master

--- config/application.config.php
+++ config/application.config.php
@@ -2,8 +2,8 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application'
-//        'Test'sxcsd
+        'Application',
+//        'Test'
     ),
     'db_data'=>array(
        'driver'=>'pdo',
